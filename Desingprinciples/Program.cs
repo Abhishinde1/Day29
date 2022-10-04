758cd5125b85764db29677002a460e3acbfdc529
AbhiShinde1 <abhishinde572@gmail.com>
2022-10-04 14:38:46 +0530

    Welcome to Design principles Programs

---
+++ Desingprinciples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Desingprinciples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Design Principles");
+        }
+    }
+}
